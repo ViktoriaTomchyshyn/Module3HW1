834d11849d6857f6b818270cc241a72fbdf27996
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2022-11-18 23:56:46 +0300

    Initial commit / without sort

---
+++ Module3HW1/Module3HW1/BaseCustomListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW1
+{
+    public abstract class BaseCustomListItem<T> : IComparable<T>
+    {
+        public int CompareTo(T other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return ToString().CompareTo(other.ToString());
+            }
+        }
+    }
+}
